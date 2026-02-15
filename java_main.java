51d3e444b84ead67e6c7ca38f5c3747e838af735
fatimaaziz27 <fatimaaziz250907@gmail.com>
2026-02-15 20:56:52 +0500

    FIXO

--- java_main.java
+++ java_main.java
@@ -1,3 +1,36 @@
 import java.util.*;
+// Output cases:
 
-//
+// Welcome to Python Bank!
+
+// Signup
+// Login
+// Exit
+// Choose an option:
+
+// if Choose an option: 1
+
+// Enter Username:
+// Enter Password:
+// Set a 4-digit PIN:
+// Account Created Successfully!
+
+// Signup
+// login
+// Exit
+// Choose an option:
+
+// if Choose an option: 2
+// Enter Username:
+// Enter Password:
+// Login Successful!
+
+// Banking Menu:
+// Deposit Money
+// Withdraw Money
+// Check Balance
+// Transaction History !
+// Transfer Money
+// Logout
+
+// Choose an option:
