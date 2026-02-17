4906cf3adaee1a18ad145fa3d660b5a1aeba2f7a
fatimaaziz27 <fatimaaziz250907@gmail.com>
2026-02-17 11:53:40 +0500

    Update java_main.java

--- java_main.java
+++ java_main.java
@@ -34,3 +34,94 @@ import java.util.*;
 // Logout
 
 // Choose an option:
+
+
+
+
+
+
+
+
+class BANK{
+    public static void main(String[] args){
+        
+    }
+}
+class bank{
+    HashMap<String,account> details =new HashMap<>();
+    void sign_up(){
+        
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter Username: ");
+        String name = sc.nextLine();
+        
+        System.out.println("Enter Password: ");
+        String passkey = sc.nextLine();
+        
+        System.out.println("Enter Initial Balance: ");
+        int balance = sc.nextInt();
+
+        System.out.println("Set a 4-digit PIN:");
+        int pin = sc.nextInt();
+        
+        if (pin >= 0000 && pin <= 9999 ){
+            System.out.println("---------");
+        }
+
+        System.out.println("acc ");
+        int acc_num = sc.nextInt();
+
+        details.put(name,new account(acc_num,name,pin,passkey,balance));
+    }
+    void login(){
+        
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter Username: ");
+        String name = sc.nextLine();
+
+        System.out.println("Enter PIN: ");
+        String pin = sc.nextLine();
+        
+        account acc = details.get(pin);
+        if (acc.details.containsKey(pin) == true){
+            System.out.println("Login Successful!");
+        }
+    }
+}
+class account extends bank{
+    Integer account_number;
+    String user_name;
+    Integer PIN;
+    String password;
+    Integer balance;
+
+    account(Integer account_number,String user_name,Integer PIN,String password,Integer balance){
+        this.balance = balance;
+        this.account_number = account_number;
+        this.PIN = PIN;
+        this.password = password;
+        this.user_name = user_name;
+    }
+    void display_info(){
+        System.out.println("Balance: "+this.balance+
+                "\nAccount Number: "+this.account_number+
+                "\nPIN: "+this.PIN +
+                "\nPassword: "+this.password+
+                "\nName: "+this.user_name);
+    }
+    void deposit(){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter deposit amount:");
+        int am = sc.nextInt();
+        this.balance+=am;
+    }
+    void withdraw(){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the amount you want to deposit:");
+        int am = sc.nextInt();
+        if (am<=balance){
+            this.balance-=am;
+            System.out.println("-------");
+        }
+    }
+}
