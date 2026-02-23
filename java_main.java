4ffa48cdfb9402c2071d01c9482b45233bbe2b8f
fatimaaziz27 <fatimaaziz250907@gmail.com>
2026-02-23 21:24:03 +0500

    Fixio

--- java_main.java
+++ java_main.java
@@ -1,3 +1,37 @@
+
+// Output cases:
+// Welcome to Python Bank!
+
+// Signup
+// Login
+// Exit
+// Choose an option:
+
+// if Choose an option: 1
+
+// Enter Username:
+// Enter Password:
+// Set a 4-digit PIN:
+// Account Created Successfully!
+
+// Signup
+// login
+// Exit
+// Choose an option:
+
+// if Choose an option: 2
+// Enter Username:
+// Enter Password:
+// Login Successful!
+
+// Banking Menu:
+// Deposit Money
+// Withdraw Money
+// Check Balance
+// Transaction History !
+// Transfer Money
+// Logout
+
 import java.util.*;
 import java.time.*;
 
@@ -166,7 +200,7 @@ class account extends bank{
         }
     }
     public String getuser_name(){
-       return user_name; 
+        return user_name;
     }
     public String getPassword(){
         return password;
@@ -175,216 +209,4 @@ class account extends bank{
         return PIN;
     }
 
-}
-// Output cases:
-// Welcome to Python Bank!
-
-// Signup
-// Login
-// Exit
-// Choose an option:
-
-// if Choose an option: 1
-
-// Enter Username:
-// Enter Password:
-// Set a 4-digit PIN:
-// Account Created Successfully!
-
-// Signup
-// login
-// Exit
-// Choose an option:
-
-// if Choose an option: 2
-// Enter Username:
-// Enter Password:
-// Login Successful!
-
-// Banking Menu:
-// Deposit Money
-// Withdraw Money
-// Check Balance
-// Transaction History !
-// Transfer Money
-// Logout
-
-import java.util.*;
-import java.time.*;
-
-class java_main{
-    public static void main(String[] args){
-        bank b1 = new bank();
-        while (true) {
-            System.out.println("1.Signup\n2.Login");
-            Scanner sc = new Scanner(System.in);
-            System.out.println("Choose an option");
-            Integer op = sc.nextInt();
-            switch(op){
-                case 1:
-                    b1.sign_up();
-                    break;
-                case 2:
-                    b1.login();
-                    break;
-            }
-            Scanner choice = new Scanner(System.in);
-            System.out.println("Do you want to continue (yes/no)?");
-            if (op.equals("No")){
-                break;
-            }
-        }
-    }
-}
-class bank{
-    HashMap<String,account> details =new HashMap<>();
-    void sign_up(){
-
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter Username: ");
-        String name = sc.nextLine();
-
-        System.out.println("Enter Password: ");
-        String passkey = sc.nextLine();
-
-        System.out.println("Enter Initial Balance: ");
-        int balance = sc.nextInt();
-
-        System.out.println("Set a 4-digit PIN:");
-        int pin = sc.nextInt();
-
-        if (pin >= 0000 && pin > 9999 ){
-            System.out.println("---------");
-        }
-
-        System.out.println("ACCOUNT NUMBER: ");
-        int acc_num = sc.nextInt();
-
-        details.put(name,new account(acc_num,name,pin,passkey,balance));
-    }
-    void login(){
-
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter Username: ");
-        String name = sc.nextLine();
-
-        System.out.println("Enter PIN: ");
-        String pin = sc.nextLine();
-
-        account acc = details.get(name);
-        if (acc.details.containsKey(name) == true){
-            System.out.println("Login Successful!");
-        }
-
-        while (true){
-            System.out.println("Banking Menu:\n" +
-                    "1. Deposit Money\n" +
-                    "2. Withdraw Money\n" +
-                    "3. Check Balance\n" +
-                    "4. Transaction History !\n" +
-                    "5. Transfer Money");
-
-            System.out.println("Enter your choice: ");
-            Integer choice = sc.nextInt();
-
-            switch(choice){
-                case 1:
-                    acc.deposit();
-                    break;
-                case 2:
-                    acc.withdraw();
-                    break;
-                case 3:
-                    acc.display_info();
-                    break;
-                case 4:
-                    System.out.println("Transfer History");
-                    break;
-                case 5:
-                    Scanner sc = new Scanner (System.in);
-                    System.out.println("Enter account name: ");
-                    String acc_name = sc.nextLine();
-                    if (acc.details.containsKey(acc_name) == true){
-                    acc.transfer_money();
-                    }
-                    else{
-                        System.out.println("Account not available.
-                        ");
-                    }
-                    break;
-            }
-
-            System.out.println("Do you want to Logout (yes/no)?");
-            String exit = sc.nextLine();
-
-            if (exit.equals("yes")){
-                System.out.println("Logout successful!");
-                break;
-            }
-        }
-    }
-}
-
-class account extends bank{
-    Integer account_number;
-    String user_name;
-    Integer PIN;
-    String password;
-    Integer balance;
-
-    account(Integer account_number,String user_name,Integer PIN,String password,Integer balance){
-        this.balance = balance;
-        this.account_number = account_number;
-        this.PIN = PIN;
-        this.password = password;
-        this.user_name = user_name;
-    }
-
-    void display_info(){
-        System.out.println("Balance: "+this.balance+
-                "\nAccount Number: "+this.account_number+
-                "\nPIN: "+this.PIN +
-                "\nPassword: "+this.password+
-                "\nName: "+this.user_name);
-    }
-
-    void deposit(){
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter deposit amount:");
-        int am = sc.nextInt();
-        this.balance+=am;
-    }
-
-    void withdraw(){
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the amount you want to withdraw:");
-        int am = sc.nextInt();
-
-        if (am<=balance){
-            this.balance-=am;
-            System.out.println("Withdraw complete");
-        }
-    }
-
-// Icomplete ----->
-
-    void transfer_money(){
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the ammount you want to tansfer: ");
-        Integer amount = sc.nextInt();
-
-        if (amount<=balance){
-            this.balance-=amount;
-
-            System.out.println("Transfered!");
-        }
-        else{
-            System.out.println("Insufficient Funds");
-
-            //         String acc_name = sc.nextLine();
-            //         if (acc.details.containsKey(acc_name) == true){
-            //         acc.transfer_money();
-        
-        }
-    }
 }
