b189b4cc109842b9daf2ec18181740fd8d3e7381
fatimaaziz27 <fatimaaziz250907@gmail.com>
2026-02-24 09:41:16 +0500

    Update java_main.java

--- java_main.java
+++ java_main.java
@@ -1,244 +1,190 @@
-
-// Output cases:
-// Welcome to Python Bank!
-
-// Signup
-// Login
-// Exit
-// Choose an option:
-
-// if Choose an option: 1
-
-// Enter Username:
-// Enter Password:
-// Set a 4-digit PIN:
-// Account Created Successfully!
-
-// Signup
-// login
-// Exit
-// Choose an option:
-
-// if Choose an option: 2
-// Enter Username:
-// Enter Password:
-// Login Successful!
-
-// Banking Menu:
-// Deposit Money
-// Withdraw Money
-// Check Balance
-// Transaction History !
-// Transfer Money
-// Logout
-
-// Output cases:
-// Welcome to Python Bank!
-
-// Signup
-// Login
-// Exit
-// Choose an option:
-
-// if Choose an option: 1
-
-// Enter Username:
-// Enter Password:
-// Set a 4-digit PIN:
-// Account Created Successfully!
-
-// Signup
-// login
-// Exit
-// Choose an option:
-
-// if Choose an option: 2
-// Enter Username:
-// Enter Password:
-// Login Successful!
-
-// Banking Menu:
-// Deposit Money
-// Withdraw Money
-// Check Balance
-// Transaction History !
-// Transfer Money
-// Logout
-
 import java.util.*;
-import java.time.*;
-
 class main{
+
+
+// 0. Exit
+// 1. View Bus Schedules
+// 2. Book Ticket
+// 3. Cancel Booking
+// 4. View My Bookings
+// Enter your choice: 1
+
+// --- Available Bus Schedules ---
+// ID: 1 | Route: north nazimabad - power house | Time: 09:00 AM | Fare: 500 | Seats Left: 30
+// ID: 2 | Route: kda - gulshan | Time: 12:00 PM | Fare: 700 | Seats Left: 30
+// ID: 3 | Route: ayesha manzil - bahria | Time: 05:00 PM | Fare: 600 | Seats Left: 30
+
+// 0. Exit
+// 1. View Bus Schedules
+// 2. Book Ticket
+// 3. Cancel Booking
+// 4. View My Bookings
+// Enter your choice: 2
+
+// --- Available Bus Schedules ---
+// ID: 1 | Route: north nazimabad - power house | Time: 09:00 AM | Fare: 500 | Seats Left: 30
+// ID: 2 | Route: kda - gulshan | Time: 12:00 PM | Fare: 700 | Seats Left: 30
+// ID: 3 | Route: ayesha manzil - bahria | Time: 05:00 PM | Fare: 600 | Seats Left: 30
+
+// Enter Bus ID to book: 2
+// Enter your name: yusra
+// Enter number of seats: 3
+// Booking successful! Total Fare: 2100
+
+// 0. Exit
+// 1. View Bus Schedules
+// 2. Book Ticket
+// 3. Cancel Booking
+// 4. View My Bookings
+// Enter your choice: 4
+
+// --- Booked Tickets ---
+// Name: yusra, Route: kda - gulshan, Time: 12:00 PM, Seats: 3, Total Fare: 2100
+
+// 0. Exit
+// 1. View Bus Schedules
+// 2. Book Ticket
+// 3. Cancel Booking
+// 4. View My Bookings
+// Enter your choice: 3
+// Enter your name to cancel booking: yusra
+// Booking cancelled successfully!
+
+// 0. Exit
+// 1. View Bus Schedules
+// 2. Book Ticket
+// 3. Cancel Booking
+// 4. View My Bookings
+// Enter your choice: 0
+// Exiting Bus Ticket Booking System...
+
+// === Code Execution Successful ===
+
     public static void main(String[] args){
-        bank b1 = new bank();
+
+        methods m1 = new methods ();
+
+        //  Enter your choice:
+
         while (true) {
-            System.out.println("1.Signup\n2.Login");
+            System.out.println("1. View Bus Schedules\n"
+                    +"2. Book Ticket"
+                    +"\n3. Cancel Booking"
+                    +"\n4. View My Bookings");
+
             Scanner sc = new Scanner(System.in);
             System.out.println("Choose an option");
             Integer op = sc.nextInt();
             switch(op){
                 case 1:
-                    b1.sign_up();
+                    
                     break;
                 case 2:
-                    b1.login();
+                    
+                    break;
+                case 3:
+                    
+                    break;
+                case 4:
+                    
                     break;
             }
             Scanner choice = new Scanner(System.in);
             System.out.println("Do you want to continue (yes/no)?");
-            String o = choice.nextLine();
-            if (o.equals("No")){
-                break;
-            }
-        }
-    }
-}
-class bank{
-    HashMap<String,account> details = new HashMap<>();
-    void sign_up(){
-
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter Username: ");
-        String name = sc.nextLine();
-
-        System.out.println("Enter Password: ");
-        String passkey = sc.nextLine();
-
-        System.out.println("Enter Initial Balance: ");
-        int balance = sc.nextInt();
-
-        System.out.println("Set a 4-digit PIN:");
-        int pin = sc.nextInt();
-
-        if (pin >= 0000 && pin <= 9999 ){
-            System.out.println("---------");
-        }
-
-        System.out.println("ACCOUNT NUMBER: ");
-        int acc_num = sc.nextInt();
-
-        details.put(name,new account(acc_num,name,pin,passkey,balance));
-    }
-    void login(){
-
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter Username: ");
-        String name = sc.nextLine();
-
-        System.out.println("Enter PIN: ");
-        String pin = sc.nextLine();
-
-        account acc = details.get(name);
-        if (acc.details.containsKey(name) == true){
-            System.out.println("Login Successful!");
-        }
-
-        while (true){
-            System.out.println("Banking Menu:\n" +
-                    "1. Deposit Money\n" +
-                    "2. Withdraw Money\n" +
-                    "3. Check Balance\n" +
-                    "4. Transaction History !\n" +
-                    "5. Transfer Money");
-
-            System.out.println("Enter your choice: ");
-            Integer choice = sc.nextInt();
-
-            switch(choice){
-                case 1:
-                    System.out.println("Enter deposit amount:");
-                    int am = sc.nextInt();
-                    acc.deposit(am);
-                    break;
-                case 2:
-                    System.out.println("Enter the amount you want to withdraw:");
-                    int am1 = sc.nextInt();
-                    acc.withdraw(am1);
-                    break;
-                case 3:
-                    acc.display_info();
-                    break;
-                case 4:
-                    System.out.println("Transfer History");
-                    break;
-                case 5:
-                    sc.nextLine();
-                    System.out.println("Enter account name: ");
-                    String acc_name = sc.nextLine();
-                    account acc_n = details.get(acc_name);
-                    if (acc_name != null){
-                        System.out.println("Enter the amount you want to transfer: ");
-                        int transfer_amount = sc.nextInt();
-                        acc.transfer_money(acc_n,transfer_amount);
-                    }
-                    break;
-            }
-            sc.nextLine();
-            System.out.println("Do you want to Logout (yes/no)?");
-            String exit = sc.nextLine();
-
-            if (exit.equals("yes")){
-                System.out.println("Logout successful!");
+            if (op.equals("No")){
+                System.out.print("====== Exiting Bus Ticket Booking System ======");
                 break;
             }
         }
     }
 }
 
-class account extends bank{
-    Integer account_number;
-    String user_name;
-    Integer PIN;
-    String password;
-    Integer balance;
+class bus_data{
 
-    account(Integer account_number,String user_name,Integer PIN,String password,Integer balance){
-        this.balance = balance;
-        this.account_number = account_number;
-        this.PIN = PIN;
-        this.password = password;
-        this.user_name = user_name;
-    }
-
-    void display_info(){
-        System.out.println("\nName: "+this.user_name+
-                "Balance: "+this.balance+
-                "\nAccount Number: "+this.account_number);
-    }
-
-    void deposit(int amm){
-        int am = amm;
-        this.balance+=am;
-    }
-
-    void withdraw(int amm){
-        int am = amm;
-
-        if (am<=balance){
-            this.balance-=am;
-            System.out.println("Withdraw complete");
-        }
-    }
-
-    void transfer_money(account acc_reciever,int amount){
-        if (amount<=balance){
-            this.withdraw(amount);
-            acc_reciever.deposit(amount);
-
-            System.out.println("Transfered: "+amount+"to "+acc_reciever.getuser_name());
-        }
-        else{
-            System.out.println("Invalid transfer or Insufficient Funds");
-        }
-    }
-    public String getuser_name(){
-        return user_name;
-    }
-    public String getPassword(){
-        return password;
-    }
-    public Integer getPIN(){
-        return PIN;
-    }
+    HashMap<String,methods> data = new HashMap<>();
 
 }
+
+class methods extends bus_data {
+
+    //  Attributes ------>
+
+    String customer_name;
+    String route;
+    String time;
+    Integer seats;
+    Integer total_fare;
+
+//  constructor ------>
+
+    methods(String name, String route, String time, Integer seats, Integer fare) {
+        this.customer_name = name;
+        this.route = route;
+        this.seats = seats;
+        this.time = time;
+        this.total_fare = fare;
+    }
+}
+
+//  1. View Bus Schedules
+
+class view_buses_Schedules(){
+
+        // Incomplete --------->
+
+        System.out.println("--- Available Bus Schedules ---"
+                +"ID: 1 | Route: north nazimabad - power house | Time: 09:00 AM | Fare: 500 | Seats Left: 30"
+                +"ID: 2 | Route: kda - gulshan | Time: 12:00 PM | Fare: 700 | Seats Left: 30"
+                +"ID: 3 | Route: ayesha manzil - bahria | Time: 05:00 PM | Fare: 600 | Seats Left: 30");
+
+    }
+
+//  2. Book Ticket
+
+class ticket_booking(){
+
+// Enter Bus ID to book: 2
+// Enter your name: yusra
+// Enter number of seats: 3
+// Booking successful! Total Fare: 2100
+
+        Scanner sc = new Scanner (System.in);
+        System.out.println("Enter Schedules ID: ");
+        Integer booking_id = sc.nextInt();
+
+        System.out.println("Enter your name: ");
+        String cus_name = sc.nextLine();
+
+        data.put(cus_name,new bus_data(cus_name,booking_id));
+    }
+
+//  3. Cancel Booking
+
+class cancel_booking(){
+
+// Enter your name to cancel booking: yusra
+// Booking cancelled successfully!
+
+        Scanner sc = new Scanner (System.in);
+        System.out.println("Enter your name: ");
+        String cus_name = sc.nextLine();
+        if (data.containsKey(cus_name) == true){
+            data.remove(cus_name);
+        }
+    }
+
+//  4. View My Bookings
+
+class display_info(){
+
+        // Name: yusra, Route: kda - gulshan, Time: 12:00 PM, Seats: 3, Total Fare: 2100
+
+        Scanner sc = new Scanner (System.in);
+        System.out.println("Enter your name: ");
+        String cus_name = sc.nextLine();
+        if (data.containsKey(cus_name) == true){
+            System.out.println("hehe");
+        } 
+}
+
+
