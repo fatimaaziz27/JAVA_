21640b21352ba060e0ea1d499967a6d15351309e
fatimaaziz27 <fatimaaziz250907@gmail.com>
2026-02-24 21:18:49 +0500

    fixio

--- java_main.java
+++ java_main.java
@@ -98,30 +98,40 @@ class main{
         }
     }
 }
+class data{
+    HashMap<String,booking_system> details = new HashMap<>();
+}
+class booking_system extends data{
 
-class booking_system{
     //  Attributes ------>
+
     String customer_name;
     String route;
     String time;
     Integer seats;
     Integer total_fare;
+    Integer ID;
     //  constructor ------>
-    booking_system(String name, String route, String time, Integer seats, Integer fare) {
+
+    booking_system(String name, String route, String time, Integer seats, Integer fare,Integer ID) {
         this.customer_name = name;
         this.route = route;
         this.seats = seats;
         this.time = time;
         this.total_fare = fare;
+        this.ID = ID;
 }
+
 //  1. View Bus Schedules method
+
     void view_buses_Schedules(){
+
         System.out.println("--- Available Bus Schedules ---"
                 +"ID: 1 | Route: north nazimabad - power house | Time: 09:00 AM | Fare: 500 | Seats Left: "+this.seats
                 +"ID: 2 | Route: kda - gulshan | Time: 12:00 PM | Fare: 700 | Seats Left: "+this.seats
                 +"ID: 3 | Route: ayesha manzil - bahria | Time: 05:00 PM | Fare: 600 | Seats Left: "+this.seats);
-
     }
+
 //  2. Book Ticket method
     void ticket_booking(){
         
@@ -129,6 +139,7 @@ class booking_system{
 // Enter your name: yusra
 // Enter number of seats: 3
 // Booking successful! Total Fare: 2100
+
         Scanner sc = new Scanner (System.in);
 
         System.out.println("Enter Bus ID: ");
@@ -141,10 +152,29 @@ class booking_system{
         System.out.println("Enter number of seats: ");
         Integer seat_num = sc.nextInt();
 
-        if (this.seats<=seat_num){
-            
+        data d1 = details.get(ID);
+        if (details.containsKey(booking_id)){
+            if(details.containsKey(seat_num)){
+                if(this.seats == seat_num || this.seats >= seat_num)
+                this.seats-=seat_num;
+            }
+            else{
+                System.out.println("only "+this.seats+"left seats");
+            }
+        }
+        else{
+            System.out.println("Invalid ID");
         }
 
+        if (booking_id == 1){
+
+        }
+        else if(booking_id == 2){
+
+        }
+        else if(booking_id == 3){
+            
+        }
     }
 
 //  3. Cancel Booking
