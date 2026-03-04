ea9afeeee3ec756fdf9e5c9f86d2e2fa6d315c07
fatimaaziz27 <fatimaaziz250907@gmail.com>
2026-03-04 22:17:06 +0500

    commit

--- java_main.java
+++ java_main.java
@@ -4,24 +4,33 @@
 // Write a Java function `safe_subtract(x: float, y: float) -> float` that subtracts two numbers and handles
 // the case where the subtraction results in a negative number.
 
-// 4. Handling Invalid Input
-// Write a Java function `parse_input(value: str) -> int` that takes a string input and converts it to an
-// integer. If the conversion fails, return the message `"Invalid input"`.
-
 // 5. Negative Numbers Handling
 // Write a Java function `add_positive_numbers(x: int, y: int) -> int` that adds two numbers but only if both
 // are positive. If either number is negative, raise a custom error: `"Both numbers must be positive"`.
 
 
-
-
-
-
+// 4. Handling Invalid Input
+// Write a Java function `parse_input(value: str) -> int` that takes a string input and converts it to an
+// integer. If the conversion fails, return the message `"Invalid input"`.
 
 import java.util.*;
     public class java_main{
 
     public static void main(String args[]) {
+
+        Scanner sc = new Scanner(System.in);
+        try{
+            System.out.println("Enter a number: ");
+            float a = sc.nextInt();
+            System.out.println("Enter a number: ");
+            float b = sc.nextInt();
+            float c = a + b;
+            System.out.println("Result:" + c);
+        }
+        catch (Exception e){
+            System.out.println("invalid input");
+        }
+
         int x =Integer.parseInt("9");
         double c = Double.parseDouble("5");
         int b = Integer.parseInt("444",16);
