7f1d35179afb09bb000ae8abfd06b8c63ba887fc
fatimaaziz27 <fatimaaziz250907@gmail.com>
2026-03-04 22:14:51 +0500

    fixi

--- java_main.java
+++ java_main.java
@@ -18,8 +18,19 @@
 
 
 
+import java.util.*;
+    public class java_main{
 
+    public static void main(String args[]) {
+        int x =Integer.parseInt("9");
+        double c = Double.parseDouble("5");
+        int b = Integer.parseInt("444",16);
 
+        System.out.println(x);
+        System.out.println(c);
+        System.out.println(b);
+    }
+}
 
 
 
