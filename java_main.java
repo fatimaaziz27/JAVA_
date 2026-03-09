471cd15a5ab414d4577fc1045e10bc720ff507ca
fatimaaziz27 <fatimaaziz250907@gmail.com>
2026-03-09 09:43:33 +0500

    Update java_main.java

--- java_main.java
+++ java_main.java
@@ -1,3 +1,121 @@
 // This java file is only for practice
 
 
+import java.util.*;
+
+// Employee Payroll System
+
+// Features
+// Add Employee
+// View Employees
+// Calculate Salary
+// Remove Employee
+// Expected Output
+
+// ==== Employee Payroll System ====
+
+// 1. Add Employee
+// 2. View Employees
+// 3. Calculate Salary
+
+// Choose option: 1
+
+// Enter Employee ID: 501
+// Enter Name: Ali
+// Enter Monthly Salary: 60000
+
+// Employee added successfully
+
+// Salary Calculation
+
+// Enter Employee ID: 501
+
+// Name: Ali
+// Monthly Salary: 60000
+// Annual Salary: 720000
+
+
+
+
+// Tech Company Meeting Scheduler
+
+// Features
+
+// View Meeting Rooms / Managers
+// Schedule Meeting
+// View My Meetings
+// Cancel Meeting
+
+// Expected Output
+// Main Menu
+// ===== Tech Company Meeting Scheduler =====
+
+// 1. View Meeting Rooms
+// 2. Schedule Meeting
+// 3. View My Meetings
+// 4. Cancel Meeting
+// 5. Exit
+
+// Choose option:
+// View Meeting Rooms
+// Choose option: 1
+
+// ------ Available Meeting Rooms ------
+
+// ID: 1 | Room: Alpha | Capacity: 10
+// ID: 2 | Room: Beta | Capacity: 6
+// ID: 3 | Room: Gamma | Capacity: 20
+// Schedule Meeting
+// Choose option: 2
+
+// Enter Room ID: 1
+// Enter Employee Name: Fatima
+// Enter Meeting Title: Project Discussion
+// Enter Date: 20-03-2026
+// Enter Time: 03:00 PM
+
+// Meeting Scheduled Successfully!
+// View My Meetings
+// Choose option: 3
+
+// Enter Employee Name: Fatima
+
+// ------ Your Meetings ------
+
+// Employee: Fatima
+// Meeting: Project Discussion
+// Room: Alpha
+// Date: 20-03-2026
+// Time: 03:00 PM
+// Cancel Meeting
+// Choose option: 4
+
+// Enter Employee Name: Fatima
+
+// Meeting cancelled successfully!
+// Extra Features (good practice)
+
+// You can also add:
+
+// Prevent double booking
+// Room already booked at this time.
+// Please choose another time slot.
+// Show meeting schedule
+// Room Alpha Schedule
+
+// 10:00 AM - Team Standup
+// 12:00 PM - Available
+// 03:00 PM - Project Discussion
+
+// This project is great practice for Java because you can use:
+
+// Classes (Meeting, Room, Employee)
+
+// HashMap or ArrayList
+
+// Time/date validation
+
+// Menu-based system
+
+
+
