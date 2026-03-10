f82ffd7ba73e0434b5df80a17d9d5fd41dbb9ad2
fatimaaziz27 <fatimaaziz250907@gmail.com>
2026-03-11 03:09:18 +0500

    fix

--- java_main.java
+++ java_main.java
@@ -296,19 +296,147 @@ class Tech_Company_Management_System{
         Integer id = sc.nextInt();
         sc.nextLine();
 
-        System.out.println("Enter Employee Name: ");
-        String name = sc.nextLine();
 
-        System.out.println("Enter Meeting Title: ");
-        String title = sc.nextLine();
 
-        System.out.println("Enter Date: ");
-        Integer date = sc.nextInt();
+        if (id == this.ID_i){
+            sc.nextLine();
+            System.out.println("Enter Employee Name: ");
+            String name = sc.nextLine();
 
-        System.out.println("Enter Time: ");
-        Integer time = sc.nextInt();
+            System.out.println("Enter Meeting Title: ");
+            String title = sc.nextLine();
 
-System.out.println("Meeting Scheduled Successfully!");
+            System.out.println("Enter Date: ");
+            Integer date = sc.nextInt();
+
+            System.out.println("Enter Time: ");
+            Integer time = sc.nextInt();
+
+    System.out.println("Meeting Scheduled Successfully!");
+
+
+
+
+
+
+
+            System.out.println("Enter your name: ");
+            String cus_name = sc.nextLine();
+            String customer_name = cus_name;
+
+            System.out.println("Enter number of seats: ");
+            Integer seat_num = sc.nextInt();
+            sc.nextLine();
+
+            if (seat_num <= this.seats_i){
+                this.seats_i-=seat_num;
+            }
+            else{
+                System.out.println("only "+this.seats_i+" seats left.");
+            }
+            int f1 = this.fare_i*seat_num;
+            this.total_fare += f1;
+
+            System.out.println();
+            System.out.println(" ==============  Your booking details  ==============\n"
+                    +"ID: "+ this.ID_i
+                    +" | Name: "+ customer_name
+                    +" | Route: "+ this.route_i
+                    +" | Time: "+ this.time_i
+                    +" | Fare: "+ this.total_fare
+                    +" | Seats: "+ seat_num);
+
+            customer_details.put(customer_name, new data(ID_i,route_i,time_i,total_fare,seat_num));
+            this.total_fare=0;
+        }
+
+        // for route two
+
+        else if (booking_id == this.ID_j){
+            sc.nextLine();
+            System.out.println("Enter your name: ");
+            String cus_name = sc.nextLine();
+            String customer_name = cus_name;
+
+            System.out.println("Enter number of seats: ");
+            Integer seat_num = sc.nextInt();
+
+            if (seat_num <= this.seats_j){
+                this.seats_j-=seat_num;
+            }
+            else{
+                System.out.println("only "+this.seats_j+" seats left.");
+            }
+            int f2 = this.fare_j*seat_num;
+            this.total_fare += f2;
+
+            System.out.println();
+            System.out.println(" ======  Your booking details  ======\n"
+                    +"ID: "+ booking_id
+                    +" | Name: "+ customer_name
+                    +" | Route: "+ this.route_j
+                    +" | Time: "+ this.time_j
+                    +" | Fare: "+ this.total_fare
+                    +" | Seats: "+ seat_num);
+
+            customer_details.put(customer_name, new data(booking_id,route_j,time_j,total_fare,seat_num));
+            this.total_fare=0;
+        }
+
+    // for route three
+
+        else if (booking_id == this.ID_k){
+            sc.nextLine();
+            System.out.println("Enter your name: ");
+            String cus_name = sc.nextLine();
+            String customer_name = cus_name;
+
+            System.out.println("Enter number of seats: ");
+            Integer seat_num = sc.nextInt();
+
+            if (seat_num <= this.seats_k){
+                this.seats_k-=seat_num;
+            }
+            else{
+                System.out.println("only "+this.seats_k+" seats left.");
+            }
+            int f3 = this.fare_k*seat_num;
+            this.total_fare += f3;
+
+            System.out.println();
+            System.out.println(" ==============  Your booking details  ==============\n"
+                    +"ID: "+ booking_id
+                    +" | Name: "+ customer_name
+                    +" | Route: "+ this.route_k
+                    +" | Time: "+ this.time_k
+                    +" | Fare: "+ this.total_fare
+                    +" | Seats: "+ seat_num);
+
+            customer_details.put(customer_name, new data(booking_id,route_k,time_k,total_fare,seat_num));
+            this.total_fare=0;
+        }
+        else{
+            System.out.println("Invalid ID");
+        }
+    }
+
+
+
+
+
+//         System.out.println("Enter Employee Name: ");
+//         String name = sc.nextLine();
+
+//         System.out.println("Enter Meeting Title: ");
+//         String title = sc.nextLine();
+
+//         System.out.println("Enter Date: ");
+//         Integer date = sc.nextInt();
+
+//         System.out.println("Enter Time: ");
+//         Integer time = sc.nextInt();
+
+// System.out.println("Meeting Scheduled Successfully!");
 
     }
 
@@ -346,6 +474,6 @@ System.out.println("Meeting Scheduled Successfully!");
         System.out.println("Enter Employee Name: ");
         String name = sc.nextLine();
         System.out.println("Meeting cancelled successfully!");
-
+        
     }
 }
